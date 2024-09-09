6afd82dedac468fbb8ff181a62b3ef76be286553
codervibe <3507557934@qq.com>
2024-09-09 08:02:01 +0800

    第一天

---
+++ com/leetcode/day1/Demo001.java
@@ -0,0 +1,44 @@
+package com.leetcode.day1;
+
+/**
+ * Created by codervibe on 2024/9/8
+ * DateTime:2024/09/08 下午5:42
+ * Description:
+ * Others:
+ */
+
+public class Demo001 {
+    //    public int removeDuplicates(int[] nums) {
+//        if (nums.length == 0) {
+//            return 0;
+//        }
+//        int fast = 1,slow = 1;
+//        while(fast < nums.length){
+//            if(nums[fast] != nums[fast -1 ]){
+//                nums[slow] = nums[fast];
+//                ++slow;
+//            }
+//            ++fast;
+//        }
+//        return slow;
+//    }
+//    public static int removeDuplicates(int[] nums) {
+//        if (nums.length == 0) {
+//            return 0;
+//        }
+//        int left = 0, len = 1;
+//        for (int i = 0; i < nums.length; i++) {
+//            if (nums[left] != nums[i]) {
+//                len++;
+//                left = i;
+//                nums[len - 1] = nums[i];
+//
+//            }
+//        }
+//        return len;
+//    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ com/leetcode/day1/Demo002.java
@@ -0,0 +1,48 @@
+package com.leetcode.day1;
+
+import java.util.*;
+
+/**
+ * Created by codervibe on 2024/9/9
+ * DateTime:2024/09/09 上午7:54
+ * Description:
+ * Others:
+ */
+
+public class Demo002 {
+    // 第一种 8ms
+//    public static List<List<String>> groupAnagrams(String[] strs) {
+//        Map<String,List<String>> map =new HashMap<>();
+//        for (String str : strs) {
+//            char[] charArray = str.toCharArray();
+//            Arrays.sort(charArray);
+//            String key = new String(charArray);
+//            List<String> list = map.getOrDefault(key,new ArrayList<String>());
+//            list.add(str);
+//            map.put(key,list);
+//        }
+//        return new ArrayList<List<String>>(map.values());
+//    }
+
+    // 第二种 7ms
+//    public static List<List<String>> groupAnagrams(String[] strs) {
+//        Map<String,List<String>> map =new HashMap<>();
+//        for (String str : strs) {
+//            char[] charArray = str.toCharArray();
+//            Arrays.sort(charArray);
+//            map.computeIfAbsent(new String(charArray),k->new ArrayList<>()).add(str);
+//
+//        }
+//        return new ArrayList<>(map.values());
+//    }
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+    }
+
+
+    public static void main(String[] args) {
+        String[] str = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> list = groupAnagrams(str);
+        System.out.println("list = " + list);
+    }
+}
